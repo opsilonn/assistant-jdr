266d25a2c94543a0e4f754c0f39e6402f18fcf0c
hugues <huguesbegeot@gmail.com>
2021-11-01 16:44:14 +0100

    commit 0 - audio

--- models/enums/EnumRouter.js
+++ models/enums/EnumRouter.js
@@ -1,6 +1,19 @@
 const EnumRouter = Object.freeze([
-  { title: 'L\'appel de Cthulhu', route: '/call-of-cthulhu', src: '/icons/icon-coc.svg' },
-  { title: 'Donjons et Dragons', route: '/dungeons-and-dragons', src: '/icons/icon-dnd.png' },
+  {
+    title: "L'appel de Cthulhu",
+    route: "/call-of-cthulhu",
+    src: "/icons/icon-coc.svg",
+  },
+  {
+    title: "Donjons et Dragons",
+    route: "/dungeons-and-dragons",
+    src: "/icons/icon-dnd.png",
+  },
+  {
+    title: "Audio",
+    route: "/audio",
+    src: "/icons/music-app.svg",
+  },
 ]);
 
 export default EnumRouter;
---
+++ pages/audio.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-container>
+    <v-btn block @click="clicked()"> Block Button </v-btn>
+  </v-container>
+</template>
+
+<script>
+// Imports
+import { Howl, Howler } from "howler";
+
+export default {
+  name: "PageAudio",
+  transition: "slide-bottom",
+
+  components: {},
+
+  props: {},
+
+  data: () => ({
+    path: "/../Formidable_Oak.mp3",
+    // path: "@/static/audio/musique/horror/Formidable_Oak.mp3",
+    // path: "https://assets.codepen.io/21542/howler-push.mp3",
+  }),
+
+  computed: {},
+
+  watch: {},
+
+  mounted() {},
+
+  methods: {
+    clicked() {
+      const sound = new Howl({
+        src: [this.path],
+      });
+
+      sound.play();
+      console.log(sound);
+    },
+  },
+
+  head() {
+    return { title: "Audio" };
+  },
+};
+</script>
