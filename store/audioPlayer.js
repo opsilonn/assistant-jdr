d34e92c8eb75ffd35150595e1ec32cef0971814f
opsilonn <huguesbegeot@gmail.com>
2022-08-26 00:29:25 +0200

    Modifie la valeur de base des volumes

--- store/audioPlayer.js
+++ store/audioPlayer.js
@@ -13,7 +13,7 @@ const state = () => ({
       play: false,
       loop: false,
       error: false,
-      volume: 1,
+      volume: 0.75,
     },
     {
       id: 2,
@@ -24,7 +24,7 @@ const state = () => ({
       play: false,
       loop: false,
       error: false,
-      volume: 1,
+      volume: 0.75,
     },
     {
       id: 3,
@@ -35,7 +35,7 @@ const state = () => ({
       play: false,
       loop: false,
       error: false,
-      volume: 1,
+      volume: 0.75,
     },
   ],
 });
